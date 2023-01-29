779274f6d276fee7294edc227ecb5441169db814
pypcio <s.tymcio98@gmail.com>
2023-01-29 14:04:44 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("testuj z nami pedalami")
+var h3=document.querySelector("h3");
+var color1=document.getElementsByClassName(".color1");
+var color2=document.getElementsByClassName(".color2");
+console.log("test");
+console.log(h3);
+console.log(color1);
+console.log(color2);
+color1.addEventListener("input",function(){
+    console.log(color1.value);
+})
+color2.addEventListener("input",function(){
+    console.log(color1.value);
+})
