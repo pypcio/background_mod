322f6d66c425d0d1002087c3a199b691613bf140
pypcio <s.tymcio98@gmail.com>
2023-01-30 14:53:59 +0100

    script update

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-var h3=document.querySelector("h3");
-var color1=document.getElementsByClassName(".color1");
-var color2=document.getElementsByClassName(".color2");
-console.log("test");
-console.log(h3);
-console.log(color1);
-console.log(color2);
-color1.addEventListener("input",function(){
-    console.log(color1.value);
+var body=document.getElementById("back_gradient");
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+var box2=document.getElementById("container-2");
+var box1=document.getElementById("container-1")
+var but=document.getElementById("enter");
+const nag=document.getElementsByClassName("tag");
+// console.log(typeof(nag.length));
+// console.log(color1);
+// console.log(color2);
+
+function modifyInput(){
+    body.style.background=
+    "linear-gradient(to right, " 
+    +color1.value
+    + ", "
+    +color2.value
+    +")";
+ }
+ function createElement(){
+  if(nag.length===0){
+    var h3=document.createElement("h3");
+    h3.appendChild(document.createTextNode("body.style.background=linear-gradient(to right, " +color1.value+ ", "+color2.value+")"));
+    h3.classList.add("tag");
+    box2.appendChild(h3);
+  }
+}
+
+color1.addEventListener("input",modifyInput)
+color2.addEventListener("input",modifyInput)
+//different variation
+// color1.addEventListener("input",function(){
+// modifyInput();
+// });
+// color2.addEventListener("input",function(){
+//   modifyInput();
+//   });
+  // createElement();
+but.addEventListener("click",function(){
+  createElement();
 })
-color2.addEventListener("input",function(){
-    console.log(color1.value);
-})
+
